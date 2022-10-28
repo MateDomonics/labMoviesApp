b43475b9e47707cfffdf88cf19ef61b390bc5859
MateDomonics <20093156@mail.wit.ie>
2022-10-28 15:48:45 +0100

    Add new reviews support.

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
-import { Link } from 'react-router-dom'
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader'
 import UpcomingMoviesPage from "./pages/upcomingMoviesPage.js";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools'
 import MoviesContextProvider from "./contexts/moviesContext";
+import AddMovieReviewPage from './pages/addMovieReviewPage'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -30,6 +30,7 @@ const App = () => {
         <MoviesContextProvider>
           <Routes>
             <Route exact path="/movies/favourites" element={<FavouriteMoviesPage />} />
+            <Route path="/reviews/form" element={<AddMovieReviewPage />} />
             <Route path="/movies/:id" element={<MoviePage />} />
             <Route path="/" element={<HomePage />} />
             <Route path="*" element={<Navigate to="/" />} />
